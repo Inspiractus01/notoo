05a61db78067cb9aaa896b55748e05123c9d25da
michalmucha <michal@instance.studio>
2025-06-09 23:46:11 +0200

    Add docker-compose and deploy script"

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /home/pi/notoo
+echo "📦 Pulling latest code from GitHub..."
+git pull origin main
+
+echo "🐳 Building Docker containers..."
+docker compose build
+
+echo "🚀 Starting containers..."
+docker compose up -d
+
+echo "✅ Deployment finished!"
